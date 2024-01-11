c2b9d2e4fe18308c1c166c8dbd70188a233bbd49
SergeiKrivko <sergik.krivko@gmail.com>
2024-01-11 12:32:02 +0300

    build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+python3 -m venv ./venv
+
+venv/bin/python3 -m pip install -r requirements.txt
+venv/bin/python3 -m pip install buildozer
+
+#sudo apt update
+apt install -y openjdk-17-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 libffi-dev libssl-dev
+pip3 install --user --upgrade Cython==0.29.33 virtualenv  # the --user should be removed if you do this in a venv
+
+# add the following line at the end of your ~/.bashrc file
+export PATH=$PATH:~/.local/bin/
+
+ls
+
+venv/bin/buildozer android debug
+
+ls
+ls .buildozer
